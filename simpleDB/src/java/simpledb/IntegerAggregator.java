5a85ca19d00964a6a26f2f5ad3bd1bc4c9913a95
Joezc <lzc814310057@163.com>
2017-06-16 16:56:06 +0800

    Project 2, Exercise 2, AVG function not done

--- simpleDB/src/java/simpledb/IntegerAggregator.java
+++ simpleDB/src/java/simpledb/IntegerAggregator.java
@@ -1,11 +1,25 @@
 package simpledb;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  * Knows how to compute some aggregate over a set of IntFields.
  */
 public class IntegerAggregator implements Aggregator {
 
     private static final long serialVersionUID = 1L;
+    private final int gbfield;
+    private Type gbfieldtype;
+    private int afield;
+    private Op op;
+    private TupleDesc td;
+    private ArrayList<Tuple> tuples;
+    private boolean isGrouped;
+
+    private ArrayList<Integer> avgNum;
+    private ArrayList<Integer> avgReminder;
 
     /**
      * Aggregate constructor
@@ -21,9 +35,26 @@ public class IntegerAggregator implements Aggregator {
      * @param what
      *            the aggregation operator
      */
-
     public IntegerAggregator(int gbfield, Type gbfieldtype, int afield, Op what) {
         // some code goes here
+        if (gbfield == Aggregator.NO_GROUPING) {
+            isGrouped = false;
+            Type[] typeAr = {Type.INT_TYPE};
+            String[] fieldAr = {"aggregateVal"};
+            this.td = new TupleDesc(typeAr, fieldAr);
+        } else {
+            isGrouped = true;
+            Type[] typeAr = {gbfieldtype, Type.INT_TYPE};
+            String[] fieldAr = {"groupVal", "aggregateVal"};
+            this.td = new TupleDesc(typeAr, fieldAr);
+        }
+        this.gbfield = gbfield;
+        this.gbfieldtype = gbfieldtype;
+        this.afield = afield;
+        this.op = what;
+        this.tuples = new ArrayList<>();
+
+
     }
 
     /**
@@ -35,6 +66,63 @@ public class IntegerAggregator implements Aggregator {
      */
     public void mergeTupleIntoGroup(Tuple tup) {
         // some code goes here
+        try {
+            if (this.isGrouped) {
+                DbIterator i = this.iterator();
+                boolean flag = false;
+                i.open();
+                while (i.hasNext()) {
+                    Tuple tmp = i.next();
+                    if (tmp.getField(0).compare(Predicate.Op.EQUALS,
+                            tup.getField(this.gbfield))) {
+                        flag = true;
+                        int newVal = this.update(tmp, tup);
+                        tmp.setField(1, new IntField(newVal));
+                    }
+                }
+                if (!flag) {
+                    Tuple newTuple = new Tuple(this.td);
+                    newTuple.setField(0, tup.getField(this.gbfield));
+                    if (this.op.equals(Op.AVG)) {
+                        newTuple.setField(1, new IntField(1));
+                    } else {
+                        newTuple.setField(1, tup.getField(this.afield));
+                    }
+                    this.tuples.add(newTuple);
+                }
+            } else {
+                DbIterator i = this.iterator();
+                i.open();
+                Tuple tmp = i.next();
+                int newVal = this.update(tmp, tup);
+                tmp.setField(0, new IntField(newVal));
+            }
+        }catch (Exception e) {
+
+        }
+    }
+
+    private int update(Tuple tmp, Tuple tup) {
+        int tmpVal = ((IntField)tmp.getField(this.afield)).getValue();
+        int tupVal = ((IntField)tup.getField(this.afield)).getValue();
+        switch (this.op) {
+            case MIN:
+                tmpVal = Math.min(tmpVal, tupVal);
+                break;
+            case MAX:
+                tmpVal = Math.max(tmpVal, tupVal);
+                break;
+            case SUM:
+                tmpVal += tupVal;
+                break;
+            case AVG:
+                tmpVal += tupVal;
+                break;
+            case COUNT:
+                tmpVal++;
+                break;
+        }
+        return tmpVal;
     }
 
     /**
@@ -46,9 +134,7 @@ public class IntegerAggregator implements Aggregator {
      *         the constructor.
      */
     public DbIterator iterator() {
-        // some code goes here
-        throw new
-        UnsupportedOperationException("please implement me for proj2");
+        return new TupleIterator(this.td, this.tuples);
     }
 
 }
